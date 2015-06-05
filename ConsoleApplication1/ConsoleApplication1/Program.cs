e70b64ec3441410609d4995ee386348be86b9ea7
MariaMendez <mariamm496@gmail.com>
2015-06-04 21:09:05 -0600

    semana 3

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -16,19 +16,20 @@ namespace ConsoleApplication1
             //System.Console.ReadKey();
             Fibonacci calculadora= new Fibonacci();
             ArrayList entrada = new ArrayList();
+            ArrayList salida = new ArrayList();
             entrada.Add(4); 
             entrada.Add(6);
             entrada.Add(20);
             entrada.Add(1);
             entrada.Add(11);
             entrada.Add(25);
-            foreach (int obj in calculadora.RecibeArreglo(entrada))
+            foreach (int obj in salida = calculadora.RecibeArreglo(entrada))
             {
 
 
+                Console.WriteLine(obj);
 
 
-          
 
 
 
