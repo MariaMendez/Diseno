cc05e9c9b8afc3acb2d7cbac6bf8113705409225
MariaMendez <mariamm496@gmail.com>
2015-06-14 12:30:13 -0600

    enums

---
+++ Enums/ConsoleApplication1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    //Esto es un codigo de porque es importante usar enums. Video1 Hola
+    class Program
+    {
+        int f;
+        static void Main(string[] args)
+        {
+            Customer[] customers= new Customer[3];
+            customers[0] = new Customer
+            {
+                Name = "Pepe",
+                Gender = 2
+
+            };
+            customers[1] = new Customer
+            {
+                Name = "Grettel",
+                Gender = 2
+
+            };
+            customers[2] = new Customer
+            {
+                Name = "Luis",
+                Gender = 0
+
+            };
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Name= {0} && Gender= {1}",customer.Name,GetGender(customer.Gender));
+            }
+             Console.ReadLine();
+        }
+            public static string GetGender(int gender)
+            {
+                    switch(gender){
+                        case 0:
+                            return "Unknown";
+                        case 1:
+                            return "Male";
+                        case 2:
+                            return "Female";
+                        default:
+                            return "Invalid number";
+                    }
+            } 
+           
+            
+        }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public int Gender { get; set; }
+        }
+
+    }
+
+
