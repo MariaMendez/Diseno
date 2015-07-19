a577b9d647b03944e4fda5538c592817293349ff
MariaMendez <mariamm496@gmail.com>
2015-07-19 13:11:03 -0600

    Practica

---
+++ ClassLibrary1/ClassLibrary1/ReglasDeNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasEnCasa
+{
+    public class ReguladorReglasDeNegocio
+    {
+        public bool InactivarElCliente(DateTime fechaDeUltimaTransaccion, DateTime fechaActual)
+        {
+            bool estaInactivo=false;
+            int diferenciaDeDias = (fechaActual - fechaDeUltimaTransaccion).TotalDays;
+            if(diferenciaDeDias>=30){
+                estaInactivo = true;
+            }
+
+            return estaInactivo;
+        }
+        public bool ValidarGarantia(int diasVencimiento)
+        {
+            bool esValidaGarantia = false;
+            if (diasVencimiento > 7)
+            {
+                esValidaGarantia = true;
+            }
+            return esValidaGarantia;
+        }
+        public bool ValidarMultiplosDelMonto(double multiplosDelMonto, double montoMinimo)
+        {
+            bool esValidoMultiplo= false;
+            double minimoMasBajo=0.01;
+            if (multiplosDelMonto == montoMinimo || multiplosDelMonto > montoMinimo || multiplosDelMonto==minimoMasBajo)
+            {
+                esValidoMultiplo = true;
+            }
+            return esValidoMultiplo;
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       int[] Values=  (int[])Enum.GetValues(typeof(Gender));
+       foreach (int values in Values)
+       {
+           Console.WriteLine(values);
+       }
+       string[] Names = Enum.GetNames(typeof(Gender));
+       foreach (string names in Names)
+       {
+           Console.WriteLine(names);
+       }
+       Console.ReadLine();
+        }
+       
+    }
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+
+    }
+}
